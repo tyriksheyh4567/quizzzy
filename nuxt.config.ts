3d29fe1ba6011900602810872301284c06953c25
Yaromir Sheydaev <tyrikaltsheyhog@gmail.com>
2025-04-19 17:49:05 +0700

    3

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-11-01',
-  devtools: { enabled: true }
+  compatibilityDate: '2025-19-04',
+  devtools: { enabled: true },
+  future: {
+    compatibilityVersion: 4
+  }
 })
